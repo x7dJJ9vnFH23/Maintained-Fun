b3c5f495b2442af26a9881e64f9506263a95f3cf
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2024-10-10 15:07:51 +0200

    Update Loader.lua
    Ion know

--- FUNC/Loader.lua
+++ FUNC/Loader.lua
@@ -3,7 +3,9 @@ local Game = game.GameId
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
 
 local scripts = {
-    [2474168535] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/WB.lua"
+    [2474168535] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/WB.lua",
+    [4924922222] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/BHA.lua",
+    [4924922222] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/BHM.lua"
 }
 
 local scriptURL = scripts[Place] or scripts[Game]
