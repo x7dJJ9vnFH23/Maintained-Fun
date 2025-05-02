511089e2248668bff7260e71b952ca1676a34019
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2025-05-02 18:41:58 +0200

    Update Loader.lua

--- FUNC/Loader.lua
+++ FUNC/Loader.lua
@@ -10,6 +10,7 @@ local scripts = {
     [130422569332103] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/BCB.lua",
     [116495829188952] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/DR2.lua",
     [70876832253163] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/DR2.lua",
+    [122678592501168] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/BE.lua",
     [126884695634066] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/GAG.lua"
 }
 
