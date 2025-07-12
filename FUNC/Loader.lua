f66f50b40856251525b82a51c205501a68d761ad
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2025-07-12 20:58:01 +0200

    Update Loader.lua

--- FUNC/Loader.lua
+++ FUNC/Loader.lua
@@ -14,7 +14,8 @@ local scripts = {
     [126884695634066] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/GAG.lua",
     [2772610559] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/FAF.lua",
     [87039211657390] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/AC.lua",
-    [4924922222] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/BRKO.lua"
+    [4924922222] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/main/FUNC/Games/BRKO.lua",
+    [95702387256198] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/SAT.lua"
 }
 
 local scriptURL = scripts[Place] or scripts[Game]
