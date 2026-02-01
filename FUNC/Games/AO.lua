8570c531662fe1732b320b2df86d2572cfd49216
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2026-02-01 22:45:44 +0100

    Update script URL in AO.lua

--- FUNC/Games/AO.lua
+++ FUNC/Games/AO.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/f49caa0733dfeb85"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/5d654878db53490c"))()
