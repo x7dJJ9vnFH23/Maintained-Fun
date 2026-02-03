03c1edc2a06905c4847b6a65f3c195a7b5a57d47
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2026-02-03 16:10:51 +0100

    Update URL for loading game script

--- FUNC/Games/CAT.lua
+++ FUNC/Games/CAT.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/45427885ee5c684f"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/4bcb676fe3e44125"))()
