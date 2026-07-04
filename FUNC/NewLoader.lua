b57f21419a086fe55756ee34f7b235754aedea9a
x7dJJ9vnFH23 <183027116+x7dJJ9vnFH23@users.noreply.github.com>
2026-07-04 21:52:34 +0200

    Update NewLoader.lua

--- FUNC/NewLoader.lua
+++ FUNC/NewLoader.lua
@@ -15,6 +15,7 @@ local scripts = {
     [97598239454123] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/GROWAG.lua",
     [77085202503540] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/GROWAG.lua",
     [133438856880402] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/GROWAG.lua",
+    [134381727982611] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/Evomon.lua",
     [92416421522960] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/Slime%20RNG.lua",
     [130594398886540] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/GH.lua",
     [124473577469410] = "https://raw.githubusercontent.com/x7dJJ9vnFH23/Maintained-Fun/refs/heads/main/FUNC/Games/BALB.lua",
